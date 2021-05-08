f3a63c573851c990abc343b30e51dc8031f2cef6
AzaharaGV <azahara1359@gmail.com>
2021-05-08 17:23:55 +0200

    Collapsable design

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+const buttonArrowDown = document.querySelector(".ico-scroll");
+const infoPage = document.querySelector(".color-design");
+const contactPage = document.querySelector(".contact-container");
+//función para esconder elementos
+function hiddenSection (event) {
+event.preventDefault(); // hace que al desclicar el botón, la función siga funcionando
+infoPage.classList.toggle("js-hidden");
+console.log("hola");
+    
+}
+buttonArrowDown.addEventListener("click", hiddenSection);
+
