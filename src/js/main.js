fcade48e591679037c13696aecaef6d9e922f134
mZubeldia <mzubeldia09@gmail.com>
2021-05-10 13:08:34 +0200

    add collapsable to all page

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,43 @@
 
 console.log('>> Ready :)');
 const buttonArrowDown = document.querySelector(".ico-scroll");
+const buttonArrowFill = document.querySelector(".fill-arrow");
+const buttonArrowShare = document.querySelector(".share-arrow");
+
 const infoPage = document.querySelector(".color-design");
-const contactPage = document.querySelector(".contact-container");
+const fillPage = document.querySelector(".fill-container");
+const sharePage = document.querySelector(".share-container");
+
 //función para esconder elementos
 function hiddenSection (event) {
 event.preventDefault(); // hace que al desclicar el botón, la función siga funcionando
+
 infoPage.classList.toggle("js-hidden");
 console.log("hola");
     
 }
 buttonArrowDown.addEventListener("click", hiddenSection);
 
+
+//RELLENA
+
+function hiddenSectionFill (event) {
+    event.preventDefault(); // hace que al desclicar el botón, la función siga funcionando
+    
+    fillPage.classList.toggle("js-hidden");
+    console.log("hola");
+        
+    }
+buttonArrowFill.addEventListener("click", hiddenSectionFill);
+
+
+// COMPARTE
+
+function hiddenSectionShare (event) {
+    event.preventDefault(); // hace que al desclicar el botón, la función siga funcionando
+    
+    sharePage.classList.toggle("js-hidden");
+    console.log("hola");
+        
+    }
+buttonArrowShare.addEventListener("click", hiddenSectionShare);
