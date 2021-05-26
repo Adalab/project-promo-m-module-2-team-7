1f761b7615b20867d4f7156c630b556638021216
mZubeldia <mzubeldia09@gmail.com>
2021-05-26 18:03:25 +0200

    try changing bckg color when clicking button landing page

---
+++ src/js/06-blaky-btn.js
@@ -0,0 +1,9 @@
+/*const blankyBtn = document.querySelector(".blanketfy-btn");
+const body = document.querySelector("body");
+
+function blanketfy(ev) {
+  ev.preventDefault();
+  body.classList.add("blanky-background");
+}
+
+blankyBtn.addEventListener("click", blanketfy);*/
