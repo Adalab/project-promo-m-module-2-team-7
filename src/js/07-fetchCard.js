f43e19a5ccc786acd56c70168bb04aff9fa4b5de
mZubeldia <mzubeldia09@gmail.com>
2021-05-27 14:08:14 +0200

    delete button blanky landing

--- src/js/07-fetchCard.js
+++ src/js/07-fetchCard.js
@@ -28,23 +28,3 @@ function handlerCreateCard(event) {
 }
 
 createBtn.addEventListener("click", handlerCreateCard);
-
-/*API URL https://awesome-profile-cards.herokuapp.com/
-
-fetch("https://awesome-profile-cards.herokuapp.com/card", {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success === false) {
-        responseElement.innerHTML = "Tienes que rellenar los datos";
-        responseElement.classList.remove("hidden");
-      } else {
-        responseElement.innerHTML = `<a href= "${data.cardURL}">Direccion`;
-        responseElement.classList.remove("hidden");
-      }
-    });
-}
-*/
