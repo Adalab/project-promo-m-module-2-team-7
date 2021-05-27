2a883d45ab2d6e2b124873ba041780df4d202029
mZubeldia <mzubeldia09@gmail.com>
2021-05-27 21:19:46 +0200

    add share twitter functionality

--- src/js/07-fetchCard.js
+++ src/js/07-fetchCard.js
@@ -1,6 +1,9 @@
 //const createBtn = document.querySelector(".js-create-card"); están en form
 //const responseElement = document.querySelector(".js-response");
 const responseElement = document.querySelector(".js-generate");
+const twitterBtn = document.querySelector(".twitter-share");
+const twitterCardLink = document.querySelector(".js-twitter-link");
+let cardLink = ""; // variable donde se guarda link generado por API tarjeta
 
 function handlerCreateCard(event) {
   event.preventDefault();
@@ -21,10 +24,22 @@ function handlerCreateCard(event) {
         responseElement.innerHTML = "Tienes que rellenar los datos";
         responseElement.classList.remove("hidden");
       } else {
-        responseElement.innerHTML = `<a href= "${res.cardURL}">${res.cardURL}`;
+        responseElement.innerHTML = `<a href= "${res.cardURL}">${res.cardURL}</a>`;
+        twitterBtn.innerHTML = `<a class="js-new-tw-link" href = "https://twitter.com/intent/tweet?text=${res.cardURL}"</a>`;
+        twitterBtn.innerHTML += `<i class="fab fa-twitter list-icon2c"></i>
+        Compartir en twitter`;
         responseElement.classList.remove("hidden");
+        /*twitterCardLink.setAttribute(
+          "href",
+          `https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&url=${data.cardURL}`
+        );*/
       }
     });
 }
 
 createBtn.addEventListener("click", handlerCreateCard);
+/*
+          responseElement.innerHTML = `<h3 class="text-set-up">La tarjeta ha sido creada:</h3>`;
+          responseElement.innerHTML += `<p><a href="${data.cardURL}" class="link-set-up js-twitter-link" target="_blank">${data.cardURL}</a></p>`;
+          responseElement.innerHTML += `<a class="twitter-button js-btn-twitter" target="_blank">
+          */
